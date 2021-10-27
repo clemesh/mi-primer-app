0e19600c908a15454deef1c78c1205c44b36b635
Clementina <cgc.351969@gmail.com>
2021-10-27 13:56:26 -0500

    Agregando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-primer-app';
+  
+  title = 'Este es mi primer componente';
+
 }
