0ad6a7589f4d78801de42c80cfaf3be1e98b7353
Cleme <cgc.351969@gmail.com>
2021-11-08 22:04:00 -0600

    event binding continuacion

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -10,8 +10,12 @@ export class PersonasComponent  {
     texto = 'Listado de personas';
     mensaje = 'persona no añadida'
     deshabilitar = false;
+    titulo = '';
     personacrear(){
       this.mensaje = 'persona añadida'
+    }
+    modificarTitulo(event: Event){
+      this.titulo = (<HTMLInputElement>event.target).value;
     }
 
   }
